e19fe4239593fa6190e6cd5738ee646c072a1602
JamBool <mailforgit2023@gmail.com>
2025-04-07 21:31:39 +0300

    refactor main: main storyboard deleted

---
+++ Dodo-base/Dodo-base/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dodo-base
+//
+//  Created by Игорь Мунгалов on 07.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
