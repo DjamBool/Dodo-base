b85f5dbe49b6ee5c9db92adede5e94e546c0b7ec
JamBool <mailforgit2023@gmail.com>
2025-04-07 22:00:50 +0300

    View Controller: добавлена таблица, настроено расположение, подписан на делегат и датасорс таблицы

--- Dodo-base/Dodo-base/ViewController.swift
+++ Dodo-base/Dodo-base/ViewController.swift
@@ -1,19 +1,57 @@
-//
-//  ViewController.swift
-//  Dodo-base
-//
+
 //  Created by Игорь Мунгалов on 07.04.2025.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .orange
+        tableView.delegate = self
+        tableView.dataSource = self
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemGray4
+        
+        setupViews()
+        setupConstraints()
     }
-
-
+}
+    
+extension ViewController {
+    func setupViews() {
+        view.addSubview(tableView)
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
 }
 
+
+
+extension ViewController:  UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
+    
+    
+}
+
+extension ViewController: UITableViewDelegate {
+    
+}
